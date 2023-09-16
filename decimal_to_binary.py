ccbccc4c8550587102c5fda97b6f7b1030c11764
InclinedTaco <103011780+InclinedTaco@users.noreply.github.com>
2023-09-16 19:29:30 +0530

    Done and dusted

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -1,32 +1,31 @@
 def dec_to_binary(n):
-    bin_num=None
+    bin_num = None
     
-    def dtbr(n,bin_num):
+    def dtbr(n, bin_num):
         if bin_num is None:
-            bin_num=""
-            
-        if n==0:
-            while len(bin_num)<8:
-                bin_num="0"+bin_num
+            bin_num = ""
+        
+        if n == 0:
+            while len(bin_num) < 8:
+                bin_num = "0" + bin_num
             return bin_num
         else:
-            rem=n%2
-            bin_num=str(rem)+bin_num
-            return dtbr(n//2,bin_num)
-            
-        bin_num=dtbr(n,None)
-        return bin_num
-        
+            rem = n % 2
+            bin_num = str(rem) + bin_num
+            return dtbr(n // 2, bin_num)
+    
+    bin_num = dtbr(n, None)
+    return bin_num
+
 # Main function
 if __name__ == '__main__':
     
     test_cases = int(input())
-    
+    x = []
+
     for case in range(1, test_cases + 1):
         n = int(input())
-        
-        bin_num=dec_to_binary(n)
-        
-        # Print the binary representation
-        print(bin_num)
-    
+        x.append(dec_to_binary(n))
+
+    for i in x:
+        print(i)

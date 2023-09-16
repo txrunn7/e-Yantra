ed674205fc13c62a6eef677e70ef6fb18a1e427e
Tarun <mtarunn491@gmail.com>
2023-09-16 22:10:53 +0530

    update

--- AP.py
+++ AP.py
@@ -1,66 +1,55 @@
 from functools import reduce
 
+
 def generate_AP(a1, d, n):
 
     AP_series = []
 
-    for i in range(0,n):
-        AP_series.append(a1+(i*d))
+    for i in range(0, n):
+        AP_series.append(a1 + (i * d))
 
     return AP_series
 
 
-test_cases=int(input())
+test_cases = int(input())
 
-if 1<=test_cases<=25:
+if 1 <= test_cases <= 25:
 
- for case in range(1, test_cases + 1):
+    for case in range(1, test_cases + 1):
         inp = input()
         values = inp.split()
 
-        a=int(values[0])
-        d=int(values[1])
-        n=int(values[2])
+        a = int(values[0])
+        d = int(values[1])
+        n = int(values[2])
 
-        if 1<=a<=100 and 1<=d<=100 and 1<=n<=100:
-         ap=generate_AP(a,d,n)
-
-         for i in ap:
-             print(i,end=" ")
-         #print('\n')
-
-
-         squared_numbers = list(map(lambda x: x ** 2, ap))
-        
-         for i in squared_numbers:
-             print(i,end=" ")
-         #print('\n')
-
-
-         sum= reduce(lambda x, y: x + y, squared_numbers)
-         print(sum)
-         
-else:
-        
-        if 1<=a<=20 and 1<=d<=20 and 1<=n<=20:
-            ap=generate_AP(a,d,n)
+        if 1 <= a <= 100 and 1 <= d <= 100 and 1 <= n <= 100:
+            ap = generate_AP(a, d, n)
 
             for i in ap:
-             print(i,end=" ")
-            #print('\n')
+                print(i, end=" ")
+            print()
 
+            squared_numbers = list(map(lambda x: x**2, ap))
 
-            squared_numbers = list(map(lambda x: x ** 2, ap))
-        
             for i in squared_numbers:
-             print(i,end=" ")
-            print('\n')
+                print(i, end=" ")
+            print()
 
-            sum= reduce(lambda x, y: x + y, squared_numbers)
+            sum = reduce(lambda x, y: x + y, squared_numbers)
             print(sum)
 
+else:
 
+    if 1 <= a <= 20 and 1 <= d <= 20 and 1 <= n <= 20:
+        ap = generate_AP(a, d, n)
 
-            
+        for i in ap:
+            print(i, end=" ")
 
-          
+        squared_numbers = list(map(lambda x: x**2, ap))
+
+        for i in squared_numbers:
+            print(i, end=" ")
+        sum = reduce(lambda x, y: x + y, squared_numbers)
+        print(sum)

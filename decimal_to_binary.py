ac4381ff7effceee04db0132307200ad127c734e
InclinedTaco <103011780+InclinedTaco@users.noreply.github.com>
2023-09-16 19:46:48 +0530

    Final

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -37,4 +37,15 @@ if __name__ == '__main__':
         print(i)
         
     else:
-        print("Does not match the given constraint")
+        for case in range(1, test_cases + 1):
+         n = int(input())
+         if n<=20:
+          x.append(dec_to_binary(n))
+        
+         else:
+            print("Does not match the given constraint")
+
+        for i in x:
+         print(i)
+        
+

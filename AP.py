53e63d23102e9c6d11404ec6300f9737c81fa716
Tarun <mtarunn491@gmail.com>
2023-09-17 10:41:32 +0530

    updt

--- AP.py
+++ AP.py
@@ -10,11 +10,9 @@ def generate_AP(a1, d, n):
 
     return AP_series
 
-
 test_cases = int(input())
 
 if 1 <= test_cases <= 25:
-
     for case in range(1, test_cases + 1):
         inp = input()
         values = inp.split()
@@ -24,31 +22,6 @@ if 1 <= test_cases <= 25:
         n = int(values[2])
 
         if 1 <= a <= 100 and 1 <= d <= 100 and 1 <= n <= 100:
-            ap = generate_AP(a, d, n)
-
-            for i in ap:
-                print(i, end=" ")
-            print()
-
-            squared_numbers = list(map(lambda x: x**2, ap))
-
-            for i in squared_numbers:
-                print(i, end=" ")
-            print()
-
-            sum = reduce(lambda x, y: x + y, squared_numbers)
-            print(sum)
-
-else:
-    for case in range(1, test_cases + 1):
-        inp = input()
-        values = inp.split()
-
-        a = int(values[0])
-        d = int(values[1])
-        n = int(values[2])
-
-        if 1 <= a <= 20 and 1 <= d <= 20 and 1 <= n <= 20:
             ap = generate_AP(a, d, n)
 
             for i in ap:

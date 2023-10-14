cbb18eb6270cc7b16eeea5c555ad9106e3d7d771
txrunn7 <mtarunn491@gmail.com>
2023-10-14 14:46:06 +0530

    updated

--- EYRC-Tasks-main/Task 2/whycon_change_coords.py
+++ EYRC-Tasks-main/Task 2/whycon_change_coords.py
@@ -2,7 +2,7 @@ from position_hold import swift #To get the current Drone Position
 
 current_drone_position = swift().drone_position
 nth_term = 0 #Index Variable for Coordinates Array
-
+error = swift().error
 #Given Coordinates
 given_coordinates = [
     [0, 0, 23],
@@ -21,6 +21,6 @@ given_coordinates = [
 next_coordinate = given_coordinates[nth_term]
 
 #plus or minus 0.2 accuracy is checked here, then drone moves to next coordinate
-if -0.2 <= current_drone_position - next_coordinate <= +0.2 and nth_term <= len(given_coordinates):
+if [1 if -0.2 < i < 0.2 else 0 for i in error] == [1, 1, 1]:
     next_coordinate = given_coordinates[nth_term]
     nth_term += 1

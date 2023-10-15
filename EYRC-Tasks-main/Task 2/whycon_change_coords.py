0fda536acfc42b37b284afeb46979fabdf2edb2f
txrunn7 <128558808+txrunn7@users.noreply.github.com>
2023-10-15 08:36:23 +0530

    Update whycon_change_coords.py

--- EYRC-Tasks-main/Task 2/whycon_change_coords.py
+++ EYRC-Tasks-main/Task 2/whycon_change_coords.py
@@ -1,26 +1,28 @@
-from position_hold import swift #To get the current Drone Position
+from position_hold import swift 
+class waypoint_coordinate_updater(swift):
+    def __init__(self):
+        super().__init__()
+        self.nthterm = 0
+        self.given_coordinates = [
+            [0, 0, 23],
+            [2, 0, 23],
+            [2, 2, 23],
+            [2, 2, 25],
+            [-5, 2, 25],
+            [-5, -3, 25],
+            [-5, -3, 21],
+            [7, -3, 21],
+            [7, 0, 21],
+            [0, 0, 19]
 
-current_drone_position = swift().drone_position
-nth_term = 0 #Index Variable for Coordinates Array
-error = swift().error
-#Given Coordinates
-given_coordinates = [
-    [0, 0, 23],
-    [2, 0, 23],
-    [2, 2, 23],
-    [2, 2, 25],
-    [-5, 2, 25],
-    [-5, -3, 25],
-    [-5, -3, 21],
-    [7, -3, 21],
-    [7, 0, 21],
-    [0, 0, 19]
-]
+        ]
+        self.run()
 
-#Setpoint which is to be changed is declared here as next_coordinate
-next_coordinate = given_coordinates[nth_term]
+    def run(self):
+        self.setpoint = self.given_coordinates[0]
+        if [1 if -0.2 < i < 0.2 else 0 for i in self.error] == [1, 1, 1]:
+            self.nth_term += 1
+            self.setpoint = self.given_coordinates[self.nth_term]
 
-#plus or minus 0.2 accuracy is checked here, then drone moves to next coordinate
-if [1 if -0.2 < i < 0.2 else 0 for i in error] == [1, 1, 1]:
-    next_coordinate = given_coordinates[nth_term]
-    nth_term += 1
+w = waypoint_coordinate_updater()
+w.run()
